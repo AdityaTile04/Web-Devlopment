e651d6daba0c8af2028eb1020d65e188416c1bf2
Aditya Tile <adityatile99@gmail.com>
2024-06-18 21:01:21 +0530

    in day 50 learned module and exports in files

--- Backend/Day_59/module and export/math.js
+++ Backend/Day_59/module and export/math.js
@@ -6,6 +6,32 @@ function evenOdd(num) {
   }
 }
 
-evenOdd(3)
+const sum = (a, b) => a + b;
+const mul = (a, b) => a * b;
+const div = (a, b) => a / b;
 
-module.exports = evenOdd;
+//! Type 1
+
+const obj = {
+  evenOdd: evenOdd,
+  sum: sum,
+  mul: mul,
+  div: div,
+  PI: 3.14,
+};
+
+module.exports = obj;
+
+//! Type 2
+
+module.exports = {
+  sum: sum,
+  mul: mul,
+  div: div,
+};
+
+//! Type 3
+
+module.exports.sum = (a, b) => a + b;
+module.exports.mul = (a, b) => a * b;
+module.exports.div = (a, b) => a / b;
--- Backend/Day_59/module and export/script.js
+++ Backend/Day_59/module and export/script.js
@@ -1,3 +1,6 @@
 const newValue = require("./math");
 
-console.log(newValue);
+console.log(newValue.div(24, 8));
+console.log(newValue.mul(25, 23));
+console.log(newValue.sum(26, 27));
+console.log(newValue.PI);
