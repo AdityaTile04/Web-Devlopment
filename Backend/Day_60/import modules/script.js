4ec037215c6b5858e8e3abff833c83cadd63bb70
Aditya Tile <adityatile99@gmail.com>
2024-06-18 23:13:42 +0530

    learn about import modules

---
+++ Backend/Day_60/import modules/script.js
@@ -0,0 +1,10 @@
+import { generate, count } from "random-words";
+import { sum, mul, div } from "./index.js";
+
+console.log(sum(2, 2));
+console.log(mul(2, 6));
+console.log(div(2, 10));
+
+
+console.log(generate(5));
+console.log(count());
