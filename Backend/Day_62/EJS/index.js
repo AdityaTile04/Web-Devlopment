1f88fb8014602b079215af5206ac6e4f1da6a9fb
Aditya Tile <adityatile99@gmail.com>
2024-06-26 18:21:06 +0530

    Complete EJS

--- Backend/Day_62/EJS/index.js
+++ Backend/Day_62/EJS/index.js
@@ -1,18 +1,41 @@
+//! EJS (Embedded Javascript Templates)
 const express = require("express");
-const path = require("path");
 const port = 3000;
+const path = require("path");
+const userData = require("./data.json");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public/css")));
+app.use(express.static(path.join(__dirname, "public/js")));
+
 app.set("views", "ejs");
+// path.join
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
+  res.send("Welcome users!");
 });
 
-app.get("/home", (req, res) => {
-  res.send("This is home page");
+app.get("/dice", (req, res) => {
+  const dice = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs", { dice });
+});
+
+app.get("/user", (req, res) => {
+  let name = "Aditya";
+  res.render("user.ejs", { name });
+});
+
+app.get("/ig/:username", (req, res) => {
+  const { username } = req.params;
+  const data = userData[username];
+  if (data) {
+    res.render("insta.ejs", { data });
+  } else {
+    res.render("error.ejs");
+  }
 });
 
 app.listen(port, () => {
