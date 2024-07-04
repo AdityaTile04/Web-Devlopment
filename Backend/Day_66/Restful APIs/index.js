60495a13f2cc94ec4a06f9d3d4728e0c6c5b9a46
Aditya Tile <adityatile99@gmail.com>
2024-07-04 23:05:07 +0530

    i learned about Restful APIs like GET,POST,PUT,PATCH & DELETE and i perform CRUD operation

--- Backend/Day_66/Restful APIs/index.js
+++ Backend/Day_66/Restful APIs/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 const port = 3000;
 
 const app = express();
@@ -11,17 +13,21 @@ app.set("views engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 
 let posts = [
   {
+    id: uuidv4(),
     username: "adityatile",
     content: "i am fullstack developer",
   },
   {
+    id: uuidv4(),
     username: "rushi123",
     content: "i am devops engineer",
   },
   {
+    id: uuidv4(),
     username: "gaurav456",
     content: "i am java fullstack developer",
   },
@@ -29,6 +35,43 @@ let posts = [
 
 app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("form.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  const { username, content } = req.body;
+  const id = uuidv4();
+  posts.push({ id, username, content });
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const post = posts.find((p) => id === p.id);
+  res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const newContent = req.body.content;
+  const post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const post = posts.find((p) => id === p.id);
+  res.render("edit.ejs");
+});
+
+app.delete("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
 });
 
 app.listen(port, () => {
