9c000bf8658671fd847440b85b7953344a4e3e5a
Aditya Tile <adityatile99@gmail.com>
2024-07-17 13:06:39 +0530

    create connection with sql.

--- Databases/SQL/Day_69/SQL with Node.JS/index.js
+++ Databases/SQL/Day_69/SQL with Node.JS/index.js
@@ -1,4 +1,25 @@
 const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "my_data",
+  password: "aditya",
+});
+
+let query = "SHOW TABLES";
+
+try {
+  connection.query(query, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+} catch (err) {
+  console.log(err);
+}
+
+connection.end();
 
 const randomUser = () => {
   return {
@@ -8,5 +29,3 @@ const randomUser = () => {
     password: faker.internet.password(),
   };
 };
-
-console.log(randomUser());
---
+++ Databases/SQL/Day_69/SQL with Node.JS/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE user (
+    id INT UNIQUE PRIMARY KEY,
+    username VARCHAR(20) UNIQUE,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(30) NOT NULL
+);
