1f6c14865491d68eb4b5f6d6427f29b7e5032c60
Aditya Tile <adityatile99@gmail.com>
2024-08-04 17:08:33 +0530

    create user schema and model and added data within it.

--- Databases/MongoDB/Day_70/index.js
+++ Databases/MongoDB/Day_70/index.js
@@ -1,11 +1,36 @@
 const mongoose = require("mongoose");
 
 async function connection() {
-  await mongoose.connect('mongodb://localhost:27017/Delta')
+  await mongoose.connect("mongodb://localhost:27017/Delta");
 }
 
-connection().then(() => {
-  console.log(`MongoDB Connected`);
-}).catch((err) => {
-  console.log(`Some Error in DB ${err}`);
-})
+connection()
+  .then(() => {
+    console.log(`MongoDB Connected`);
+  })
+  .catch((err) => {
+    console.log(`Some Error in database`);
+  });
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+
+const user1 = new User({ name: "Aditya", email: "aditya@gmail.com", age: 19 });
+const user2 = new User({ name: "Rushi", email: "rushi@gmail.com", age: 20 });
+
+//! Insert only one user at a time
+user2
+  .save()
+  .then((res) => {
+    console.log(`Data Added to Database`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//! Inserting multiple
