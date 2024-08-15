79e2cd93dd994080b9a52fe083b9d4b431e699c0
Aditya Tile <adityatile99@gmail.com>
2024-08-15 22:24:15 +0530

    in this get route i shows the more details of individual places

--- Projects/Day_72/AirBnb/main.js
+++ Projects/Day_72/AirBnb/main.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 async function connection() {
   await mongoose.connect("mongodb://localhost:27017/AirBnb");
@@ -21,11 +22,16 @@ connection()
     console.log(err);
   });
 
-
 // Index Route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
+});
+
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
 });
 
 app.listen(PORT, (req, res) => {
