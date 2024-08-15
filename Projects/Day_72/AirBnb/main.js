2badf613353b8da9e80ddf8c8142324f117c7587
Aditya Tile <adityatile99@gmail.com>
2024-08-15 21:43:38 +0530

    create index route in this route i show all my listings of places

--- Projects/Day_72/AirBnb/main.js
+++ Projects/Day_72/AirBnb/main.js
@@ -1,11 +1,32 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const Listing = require("./models/listing");
+const mongoose = require("mongoose");
+const path = require("path");
 const PORT = 4000;
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
+async function connection() {
+  await mongoose.connect("mongodb://localhost:27017/AirBnb");
+}
+
+connection()
+  .then(() => {
+    console.log(`Database Connected`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+// Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
 
 app.listen(PORT, (req, res) => {
   console.log(`Server running on port ${PORT}`);
