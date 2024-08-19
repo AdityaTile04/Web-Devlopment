797cd4f082e9a76f9b1ea4282cc39001df8fe2e1
Aditya Tile <adityatile99@gmail.com>
2024-08-19 15:58:05 +0530

    Handling the error

---
+++ middleware/Day_73/errorHandling.js
@@ -0,0 +1,22 @@
+//! Error Handling (Express Default)
+
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.use((req, res, next) => {
+  const { token } = req.query;
+  if (token === "giveaccess") {
+    next();
+  } else {
+    throw new Error("ACCESS DENIED!");
+  }
+});
+
+app.get("/api", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- middleware/Day_73/index.js
+++ middleware/Day_73/index.js
@@ -13,6 +13,31 @@ app.use((req, res, next) => {
   next();
 });
 
+//! Using next()
+// it is must to pass control to the next middleware function.
+
+const checkToken = (req, res, next) => {
+  console.log(`hi, i'm middleware 2`);
+  next();
+};
+
+app.use("/api", checkToken, (req, res, next) => {
+  const { token } = req.query;
+  if (token === "giveaccess") {
+    next();
+  } else {
+    res.send("ACCESS DENIED");
+  }
+});
+
+app.get("/api", (req, res) => {
+  res.send("Data");
+});
+
+app.use("/random", (req, res, next) => {
+  console.log(`i'm only for random`);
+});
+
 app.get("/", (req, res) => {
   res.send("root is working");
 });
@@ -23,14 +48,4 @@ app.get("/demo", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-//! Using next()
-// it is must to pass control to the next middleware function.
-
-// example
-
-app.use((req, res, next) => {
-  console.log(`hi, i'm middleware 2`);
-  next();
 });
