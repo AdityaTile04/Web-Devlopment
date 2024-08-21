b12d6476e205cc1eccbf989f0c19be74194c6076
Aditya Tile <adityatile99@gmail.com>
2024-08-21 20:30:49 +0530

    create custome errors

--- Error/Day_74/errorHandler.js
+++ Error/Day_74/errorHandler.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const port = 4000;
+const ExpressError = require("./ExpressError");
 
 const app = express();
 
 // Error Handling Middleware (Custom)
-app.use((err, req, res, next) => {
-  console.log(err);
+
+app.use("/api", (req, res, next) => {
+  const { token } = req.query;
+
+  if (token === "access") {
+    next();
+  } else {
+    throw new ExpressError(401, "ACCESS DENIED");
+  }
+});
+
+app.get("/api", (req, res) => {
+  res.send("Data");
 });
 
 app.get("/err", (req, res) => {
   abc = abc;
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message } = err;
+  res.status(status).send(message);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
