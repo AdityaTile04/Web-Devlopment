663574c61d5bf1f5325a099a255493455c458cc0
Aditya Tile <adityatile99@gmail.com>
2024-09-22 09:10:26 +0530

    add marker and popup on map

--- Projects/Day_72/AirBnb/public/js/map.js
+++ Projects/Day_72/AirBnb/public/js/map.js
@@ -1,6 +1,17 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  center: [73.7898, 19.9975], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 9, // starting zoom
 });
+
+const marker = new mapboxgl.Marker()
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 })
+      .setLngLat(listing.geometry.coordinates)
+      .setHTML(
+        `<h4>${listing.location}</h4><p>Exact Location will be provided after booking</p>`
+      )
+  )
+  .addTo(map);
