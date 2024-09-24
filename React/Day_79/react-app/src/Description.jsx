baa25449c26ff7cd8924199cfe93a1aa1ed5b75b
Aditya Tile <adityatile99@gmail.com>
2024-09-24 19:58:59 +0530

    create first component

---
+++ React/Day_79/react-app/src/Description.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Description() {
+  return <h4>This is the description</h4>;
+}
+
+export default Description;
---
+++ React/Day_79/react-app/src/Title.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Title() {
+  return <h1>This is the title</h1>;
+}
+
+export default Title;
