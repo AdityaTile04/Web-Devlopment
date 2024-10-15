7b88c7cb64775432e7f2c7bace501d39085e39f9
Aditya Tile <adityatile99@gmail.com>
2024-10-15 21:43:02 +0530

    started creating comment module

--- React/Day_84/react-form/src/App.jsx
+++ React/Day_84/react-form/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Form from "./Form";
+import Comment from "./Comment";
 
 export default function App() {
   return (
     <div>
-      <Form />
+      {/* <Form /> */}
+      <Comment />
     </div>
   );
 }
---
+++ React/Day_84/react-form/src/Comment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Comment() {
+  return (
+    <div>
+        <h1>Give a comment</h1>
+    </div>
+  )
+}
