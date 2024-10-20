c7363277adcad43564cea8422630f211c45bbd37
Aditya Tile <adityatile99@gmail.com>
2024-10-20 11:04:17 +0530

    add validations using formik

--- React/Day_84/react-form/src/Comment.jsx
+++ React/Day_84/react-form/src/Comment.jsx
@@ -1,43 +1,74 @@
 import React, { useState } from "react";
+import { useFormik } from "formik";
+
+const validate = (values) => {
+  const errors = {};
+  if (!values.username) {
+    errors.username = "Username cannot be empty";
+  }
+  if (!values.comment) {
+    errors.comment = "comment cannot be empty";
+  }
+  if (!values.rating) {
+    errors.rating = "Rating cannot be empty";
+  }
+
+  return errors;
+};
 
 export default function Comment({ addComment }) {
-  let [formData, setFormData] = useState({
-    username: "",
-    comment: "",
-    rating: 5,
-  });
+  // let [formData, setFormData] = useState({
+  //   username: "",
+  //   comment: "",
+  //   rating: 5,
+  // });
 
-  const handleChange = (e) => {
-    setFormData((cd) => {
-      return { ...cd, [e.target.name]: e.target.value };
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-    addComment(formData)
-
-    setFormData({
+  const formik = useFormik({
+    initialValues: {
       username: "",
       comment: "",
-      rating: "",
-    });
-  };
+      rating: 5,
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  // const handleChange = (e) => {
+  //   setFormData((cd) => {
+  //     return { ...cd, [e.target.name]: e.target.value };
+  //   });
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(formData);
+  //   addComment(formData);
+
+  //   setFormData({
+  //     username: "",
+  //     comment: "",
+  //     rating: "",
+  //   });
+  // };
 
   return (
     <div>
       <h1>Give a comment</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <label htmlFor="username">Username : </label>
         <input
           id="username"
           type="text"
           name="username"
           placeholder="username"
-          value={formData.username}
-          onChange={handleChange}
+          value={formik.values.username}
+          onChange={formik.handleChange}
         />{" "}
+        {formik.errors.username ? (
+          <div style={{ color: "red" }}>{formik.errors.username}</div>
+        ) : null}
         <br />
         <br />
         <label htmlFor="comment">Comment Here : </label>
@@ -47,9 +78,12 @@ export default function Comment({ addComment }) {
           cols={22}
           rows={3}
           placeholder="comment here"
-          value={formData.comment}
-          onChange={handleChange}
+          value={formik.values.comment}
+          onChange={formik.handleChange}
         ></textarea>
+        {formik.errors.comment ? (
+          <div style={{ color: "red" }}>{formik.errors.comment}</div>
+        ) : null}
         <br />
         <br />
         <label htmlFor="rating">Rating : </label>
@@ -60,12 +94,15 @@ export default function Comment({ addComment }) {
           placeholder="rating"
           min={1}
           max={5}
-          value={formData.rating}
-          onChange={handleChange}
+          value={formik.values.rating}
+          onChange={formik.handleChange}
         />
+        {formik.errors.rating ? (
+          <div style={{ color: "red" }}>{formik.errors.rating}</div>
+        ) : null}
         <br />
         <br />
-        <button>Add Comment</button>
+        <button type="submit">Add Comment</button>
       </form>
     </div>
   );
