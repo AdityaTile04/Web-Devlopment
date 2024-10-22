d9b04e21d707ccb1bf130d21abd9688f148eb489
Aditya Tile <adityatile99@gmail.com>
2024-10-22 17:30:33 +0530

    in day 86 i started learning tailwind css

---
+++ Tailwind CSS/Day_86/tailwind-using-react/src/Demo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Demo() {
+  return (
+    <div>
+      <h1 className="text-red-700">Hello</h1>
+      <p className="text-yellow-500 border rounded-lg bg-red-50 my-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
+        aspernatur quas reiciendis dolore repellat facilis perspiciatis ad
+        commodi nemo voluptas tenetur voluptatem, excepturi nobis alias!
+        Aspernatur aliquam ducimus quas! Cumque voluptatum, tempore quisquam
+        saepe pariatur atque omnis doloribus nulla. Dolor voluptatem
+        voluptatibus rerum error. Doloremque reprehenderit labore pariatur
+        repudiandae iure!
+      </p>
+    </div>
+  );
+}
+
+export default Demo;
