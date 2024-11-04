7f636750c6bac87dbdfd08662fcf63fb1c9bc95a
Aditya Tile <adityatile99@gmail.com>
2024-11-04 20:40:35 +0530

    complete todo using react-redux and redux-toolkit

--- Redux/Day_87/redux-and-redux-toolkit/src/App.jsx
+++ Redux/Day_87/redux-and-redux-toolkit/src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import './App.css'
-import ReduxTodo from './components/ReduxTodo'
+import "./App.css";
+import ReduxTodo from "./components/ReduxTodo";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 function App() {
   return (
     <div>
-      <ReduxTodo />
+      <Provider store={store}>
+        <ReduxTodo />
+      </Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Redux/Day_87/redux-and-redux-toolkit/src/components/ReduxTodo.jsx
+++ Redux/Day_87/redux-and-redux-toolkit/src/components/ReduxTodo.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import AddForm from "./AddForm";
+import { deleteTodo } from "../features/todo/todoSlice";
 
 function ReduxTodo() {
+  const todos = useSelector((state) => state.todos);
+  console.log(todos);
+
+  const dispatch = useDispatch();
+
+  const deleteHandler = (id) => {
+    console.log(id);
+    dispatch(deleteTodo(id));
+  };
+
   return (
-    <div>ReduxTodo</div>
-  )
+    <>
+      <AddForm />
+      <h2>Todos</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.task}
+            <button onClick={() => deleteHandler(todo.id)}>Delete Task</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-export default ReduxTodo
+export default ReduxTodo;
